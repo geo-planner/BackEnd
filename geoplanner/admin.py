e2203c6e7d38bf908a86263030e7ac527ff801ac
MalickiJacek <Jacek.malicki1208@gmail.com>
2026-06-03 14:49:51 +0200

    feat: admin panel init

--- geoplanner/admin.py
+++ geoplanner/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import RouteType, VehicleType, RouteStatus, Depot, Job, Vehicle, Route, RouteStop
 # Register your models here.
+# przykłady - do dopracowania jak będziemy mieć więcej danych do wyświetlenia
+admin.site.register(RouteType)
+admin.site.register(RouteStatus)
+admin.site.register(Depot)
+@admin.register(VehicleType)
+class VehicleTypeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['job_code', 'address', 'user', 'demand','created_at']
+    list_filter = ['user']
+    search_fields = ['address', 'job_code']
+
+admin.site.register(Vehicle)
+admin.site.register(Route)
+admin.site.register(RouteStop)
