84fdc4bea94cca1121f4244db9ebbdc0c0a9718d
MalickiJacek <Jacek.malicki1208@gmail.com>
2026-06-03 15:00:34 +0200

    feat: authentication

--- geoplanner/views.py
+++ geoplanner/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'geoplanner/home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'geoplanner/register.html', {'form': form})
